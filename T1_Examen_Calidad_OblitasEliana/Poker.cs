d9aaaca0529d068384bac36fff9e8c7e9502bc5c
Eliana Oblitas <70211070+Eliana2234@users.noreply.github.com>
2021-09-10 21:50:38 -0500

    ExamenT1

---
+++ T1_Examen_Calidad_OblitasEliana/Poker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1_Examen_Calidad_OblitasEliana
+{
+    internal class Poker
+    {
+        public Poker()
+        {
+            public string CombrobarJugadores()
+            {
+                return "I";
+            }
+        }
+    }
+}
---
+++ T1_Examen_Calidad_OblitasEliana/PokerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T1_Examen_Calidad_OblitasEliana
+{
+    class PokerTest
+    {
+    }
+}
